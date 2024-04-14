d9f8690f92fc8a18cbe423d21b05a367a724c6fa
Me <lukagujejiani@gmail.com>
2024-04-14 15:48:54 +0400

    Initial Commit

---
+++ UICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionView
+//
+//  Created by Luka Gujejiani on 14.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
